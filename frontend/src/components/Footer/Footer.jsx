a24b1639eadc47cc1cfc8f872e0aab3b6e650f1f
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-01 02:18:08 +0000

    short

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,6 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../../assets/images/logo.png"
 import {RiLinkedinFill} from 'react-icons/ri'
+import {AiFillYoutube, AiFillGithub, AiOutlineInstagram} from 'react-icons/ai'
+
+const socialLinks = [
+  {
+    path: "https://www.youtube.com/c/CodingWithMuhib",
+    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.youtube.com/c/CodingWithMuhib",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.youtube.com/c/CodingWithMuhib",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.youtube.com/c/CodingWithMuhib",
+    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/",
+    display: "About Us",
+  },
+  {
+    path: "/services",
+    display: "Home",
+  },
+]
 
 const Footer = () => {
   return (
