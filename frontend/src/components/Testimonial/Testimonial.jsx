a767bc7128c0cc11866ee597c3b97fdcf76e4c7e
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-01 01:15:11 +0000

    short

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -9,8 +9,34 @@ import {HiStar} from 'react-icons/hi'
 const Testimonial = () => {
   return (
     <div className="mt-[30px] lg:mt-[55px]">
-        <Swiper modules={[Pagination]} spaceBetween={30} slidesPerView={1} pagination={{ clickable:true }}>
-
+        <Swiper 
+            modules={[Pagination]} 
+            spaceBetween={30} 
+            slidesPerView={1} 
+            pagination={{ clickable:true }}
+            breakpoints={{ 
+                640: {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                },
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                },
+            }}
+        >
+            <SwiperSlide>
+                <div className="py-[30px] px-5 rounded-3">
+                    <div className="flex items-center gap-[13px]">
+                        <img src={patientAvatar} alt="" />
+                        <div><h4 className="text-[18px] leading-[30px] font-semibold text-headingColor">Muhibur Rahman</h4></div>
+                    </div>
+                </div>
+            </SwiperSlide>
         </Swiper>
     </div>
   )
