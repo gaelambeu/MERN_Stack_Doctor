d98b75a03ddcd82ba6d0baef4c505d74c52df75e
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-01 00:26:13 +0000

    short

--- frontend/src/components/Faq/FaqItem.jsx
+++ frontend/src/components/Faq/FaqItem.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { useState } from "react"
 
-const FaqItem = () => {
+const FaqItem = ({item}) => {
   return (
-    <div>FaqItem</div>
+    <div className="p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5
+    cursor-pointer ">
+        <div className="flex items-center justify-between gap-5">
+            <h4 className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor ">
+                {item.question}
+            </h4>
+        </div>
+    </div>
   )
 }
 
