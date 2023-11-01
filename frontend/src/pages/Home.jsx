f3633f6eba7259ef52a69d9da87d23d938e717f6
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-01 00:53:29 +0000

    short

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -310,6 +310,23 @@ const Home = () => {
 
 
 
+      {/* =================== testimonial ==================== */}
+        <section>
+          <div className="container">
+            <div className="xl:w-[470px] mx-auto ">
+                <h2 className="heading text-center">What our patient say</h2>
+                <p className="text__para text-center">
+                  World-class care for everyone. Our health System offers unmatched, expert health care.
+                </p>
+              </div>
+          </div>
+        </section>
+      {/* =================== testimonial end ==================== */}
+
+
+
+
+
     </>
   )
 }
