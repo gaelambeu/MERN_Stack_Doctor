19495b656e0869a56e39e02e91da83b5306ecab1
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-01 03:01:17 +0000

    short

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -100,15 +100,60 @@ const Footer = () => {
             <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
               Quick Links
             </h2>
+
+            <ul>
+              {quickLinks01.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link 
+                    to={item.path} 
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
 
-          <ul>
-            {quickLinks01.map((item, index) => (
-              <li key={index} className="mb-4">
-                <Link to={item.path}>{item.display}</Link>
-              </li>
-            ))}
-          </ul>
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I want to:
+            </h2>
+
+            <ul>
+              {quickLinks02.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link 
+                    to={item.path} 
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Support
+            </h2>
+
+            <ul>
+              {quickLinks03.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link 
+                    to={item.path} 
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          
         </div>
       </div>
     </footer>
