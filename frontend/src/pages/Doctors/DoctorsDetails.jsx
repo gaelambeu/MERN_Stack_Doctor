331a5b52e2915ec6e01d3f8e81f58dccb85af8a1
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-15 03:35:18 +0000

    short

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -65,7 +65,7 @@ const DoctorsDetails = () => {
                 tab==='about' && <DoctorsAbout />
               }
               {
-                tab==='about' && <Feedback />
+                tab==='feedback' && <Feedback />
               }
             </div>
 
