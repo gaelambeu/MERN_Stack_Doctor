a0963487289b35791d6855a1355ee32d77f14b20
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-15 02:49:10 +0000

    short

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -9,9 +9,14 @@ const DoctorsDetails = () => {
 
           <div className="md:col-span-2">
             <div className="flex items-center gap-5">
-              <figure>
-                <img src={doctorImg} alt="" />
+              <figure className="max-w-[200px] max-h-[200px]">
+                <img src={doctorImg} alt="" className="w-full"/>
               </figure>
+
+              <div>
+                <span className="bg-[#CCF0F3] text-blue-400 py-1 px-6 lg:py-2 lg:px-6 text-[12px]
+                leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">Surgeon</span>
+              </div>
             </div>
           </div>
 
