71bcad84d582f1d219bfabad9481aa9d08ef5798
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-15 02:17:19 +0000

    short

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -11,9 +11,18 @@ const Doctors = () => {
               justify-between">
                 <input type="search" className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none 
                   cursor-pointer placeholder:text-textColor"
-                  placeholder="Search Doctor" />
+                  placeholder="Search Doctor" 
+                />
+                <button className="btn mt-0 rounded-[0px] rounded-r-md">
+                  Search
+                </button>
             </div>
         </div>
+      </section>
+
+
+      <section>
+        
       </section>
     </>
   )
