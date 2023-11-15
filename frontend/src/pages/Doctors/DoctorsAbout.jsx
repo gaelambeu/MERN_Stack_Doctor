2283684d129f79402737323b86546e5e27b3bfbb
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-15 03:49:31 +0000

    short

--- frontend/src/pages/Doctors/DoctorsAbout.jsx
+++ frontend/src/pages/Doctors/DoctorsAbout.jsx
@@ -13,7 +13,19 @@ const DoctorsAbout = () => {
         <p className="text_para">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, porro sequi. Nobis, hic asperiores.
           Itaque optio, porro illum officiis placeat dolore nesciunt, in dicta quae qui blanditiis soluta est pariatur?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos beatae, totam voluptatibus eveniet, voluptatum 
+          saepe labore cumque quaerat voluptatem culpa tempora, at itaque neque aliquid molestias eum possimus architecto sunt!
         </p>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Education
+        </h3>
+
+        <ul className="pt-4 md:p-5">
+
+        </ul>
       </div>
     </div>
   )
