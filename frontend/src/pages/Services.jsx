5a431f1da99ebd663da42d6fe61e46793a2c7802
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-15 02:03:36 +0000

    short

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
   return (
     <section>
       <div className="container">
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] ">
           {services.map((item, index ) => (
               <ServiceCard item={item} index={index} key={index} />
           ))}
