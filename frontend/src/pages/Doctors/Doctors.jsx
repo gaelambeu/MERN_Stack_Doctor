fedaed0610b8472b527c700d041c7576e95ff25b
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-15 02:22:28 +0000

    short

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { doctors } from '../../assets/data/doctors'
+import DoctorCard from '../../components/Doctors/DoctorCard'
 
 const Doctors = () => {
   return (
@@ -22,7 +24,13 @@ const Doctors = () => {
 
 
       <section>
-        
+        <div className="container">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {doctors.map( doctor => (
+                <DoctorCard key={doctor.id} doctor={doctor}/>
+            ))}
+          </div>
+        </div>
       </section>
     </>
   )
