ef0529444c4ba2fd75b328bc25144d3d8c998a68
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-15 03:41:25 +0000

    short

--- frontend/src/pages/Doctors/DoctorsAbout.jsx
+++ frontend/src/pages/Doctors/DoctorsAbout.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const DoctorsAbout = () => {
   return (
-    <div>DoctorsAbout</div>
+    <div>
+      <div>
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+          About of
+          <span className="text-blue-400 font-bold text-[24px] leading-9">
+            Muhibur Rahman
+          </span>
+        </h3>
+        <p className="text_para">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, porro sequi. Nobis, hic asperiores.
+          Itaque optio, porro illum officiis placeat dolore nesciunt, in dicta quae qui blanditiis soluta est pariatur?
+        </p>
+      </div>
+    </div>
   )
 }
 
