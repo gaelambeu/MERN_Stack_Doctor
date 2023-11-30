f601db63719b6cd056b48a29b69d5955d9653ccf
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-30 19:47:18 +0000

    short

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const FeedbackForm = () => {
   return (
-    <div>FeedbackForm</div>
+    <form action="">
+        <div>
+            <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+                How would you rate the overall experience?
+            </h3>
+
+            <div>
+                {[...Array(5).keys()].}
+            </div>
+        </div>
+    </form>
   )
 }
 
