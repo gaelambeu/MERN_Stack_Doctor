732d36bc94e76e6b1adc9dd8d4970ac28c327d58
GAEL AMBEU <ambeugael128@gmail.com>
2023-11-30 19:07:26 +0000

    short

--- frontend/src/pages/Doctors/DoctorsAbout.jsx
+++ frontend/src/pages/Doctors/DoctorsAbout.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { formateDate } from '../../utils/formateDate'
 
 const DoctorsAbout = () => {
--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import avatar from "../../assets/images/avatar-icon.png"
+import { formateDate } from '../../utils/formateDate'
+
 
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+    <div>
+        <div className="mb-[50px]">
+            <h4 className="text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]">
+                All reviews (272)
+            </h4>
+
+            <div className="flex justify-between gap-10 mb-[30px]">
+                <div className="flex gap-3">
+                    <figure className="w-10 h-10 rounded-full">
+                        <img className="w-full" src={avatar} alt="" />
+                    </figure>
+
+                    <div>
+                        <h5 className="text-[16px] leading-6 text-primaryColor font-bold">
+                            Ali Ahmed
+                        </h5>
+                        <p className="text-[14px] leading-6 text-textColor">
+                            {formateDate("02-14-2023")}
+                        </p>
+                        <p className="text__para mt-3 font-medium text-[15px]">
+                            Good services, highly recommended
+                        </p>
+                    </div>
+                </div>
+
+                <div className="flex gap-1">
+                    {[...Array(5).keys]}
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
