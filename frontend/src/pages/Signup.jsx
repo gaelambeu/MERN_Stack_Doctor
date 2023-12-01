06d8d45c7c1f8033166302c435e6629b75acc52b
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-01 16:47:24 +0000

    short

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -29,6 +29,10 @@ const Signup = () => {
     console.log(file)
   }
 
+  const submitHandler = async event => {
+    event.preventDefault()
+  }
+
   return (
     <section className="px-5 xl:px-0">
       <div className="max-w-[1170px] mx-auto">
@@ -46,7 +50,7 @@ const Signup = () => {
               Create an <span className="text-primaryColor">account</span>
             </h3>
 
-            <form >
+            <form onSubmit={submitHandler}>
               <div className="mb-5">
                 <input 
                   type="name" 
