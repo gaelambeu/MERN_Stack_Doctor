cb2dd00a574dade0d64477d322099ffcc3fd4cda
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-01 13:14:23 +0000

    short

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className="px-5 lg:px-0">
+      <div className="w-full max-w-[570px] mx"></div>
+    </section>
   )
 }
 
