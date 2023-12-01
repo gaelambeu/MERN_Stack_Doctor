6e0575e1d0d6f7dbf58d60acd18c8501302d0cc5
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-01 15:25:28 +0000

    short

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <section className="px-5 xl:px-0">
+      <div className="max-w-[1170px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          {/* ==============   img box     ==============*/}
+          <div className="hidden lg:block bg-primaryColor rounded-l-lg"></div>
+        </div>
+      </div>
+    </section>
   )
 }
 
