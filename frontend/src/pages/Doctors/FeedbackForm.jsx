6e8bbb1c4d4089cd25a12d6b3de07d1f8902eb4f
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-01 12:35:19 +0000

    short

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -5,6 +5,14 @@ const FeedbackForm = () => {
 
     const [rating, setRating] = useState(0)
     const [hover, setHover] = useState(0)
+    const [reviewText, setReviewText] = useState("")
+
+    const handleSubmitReview = async e=> {
+        e.preventDefault()
+
+        // later we will use our api
+    }
+
 
   return (
     <form action="">
@@ -49,8 +57,15 @@ const FeedbackForm = () => {
                Share your feedback or suggestions
             </h3>
 
-            <textarea className="bo" cols="30" rows="10"></textarea>
+            <textarea className="border border-solid border-[#0066ff34] focus:outline outline-primaryColor
+            w-full px-4 py-3 rounded-md"
+             rows="5" 
+             placeholder="Write your message"
+             onChange={(e)=>setReviewText(e.target.value)}
+             ></textarea>
         </div>
+
+        <button type="submit" onClick={handleSubmitReview} className="btn">Submit Feedback</button>
     </form>
   )
 }
