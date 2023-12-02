47920cb6eef696a8463d6e357c0c37c901e4659c
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-02 16:40:26 +0000

    short

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,21 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.port || 8000
+
+const corsOptions = {
+    origin:true
+}
+
+app.get('/', (req,res)=>{
+    res.send('Api is working')
+})
+
+
+app.listen(port, ()=> {
+    console.log('Server is running on port:' + port);
+})
