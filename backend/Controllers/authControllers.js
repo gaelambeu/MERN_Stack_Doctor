e98789f392bca01cd918b602a804a3b7d49c4d06
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-02 17:40:26 +0000

    short

--- backend/Controllers/authControllers.js
+++ backend/Controllers/authControllers.js
@@ -1,7 +1,19 @@
+import User from '../models/UserSchema.js'
+import Doctor from '../models/DoctorSchema.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+
 
 export const register =async(req, res) =>{
+    
+    const {email, password, name, role, photo, gender} = req.body
+
     try {
         
+        let user = null
+
+
+
     } catch (err) {
         
     }
