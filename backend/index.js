19831d6a165b734b434fe034664f1aef3065cc8d
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-02 17:17:23 +0000

    short

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,41 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.port || 8000
+const app = express();
+const port = process.env.PORT || 8000;
 
 const corsOptions = {
-    origin:true
-}
+  origin: true,
+  // Ajoutez d'autres options CORS si nécessaire
+};
 
-app.get('/', (req,res)=>{
-    res.send('Api is working')
-})
+app.get("/", (req, res) => {
+  res.send("Api is working");
+});
 
-// database connection
-mongoose.set('strictQuery', false)
-const connectDB = async() => {
-    try {
-        mongoose.connect(process.)
-    } catch (err) {
-        
-    }
-}
+// Connexion à la base de données
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB database is connected");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1); // Arrête le processus en cas d'échec de connexion
+  }
+};
 
-// middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors(corsOptions))
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
 
-app.listen(port, ()=> {
-    console.log('Server is running on port:' + port);
-})
+app.listen(port, () => {
+  connectDB();
+  console.log("Server is running on port: " + port);
+});
