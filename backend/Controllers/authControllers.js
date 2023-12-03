2806cbbcd84db1146cb315167cc623f449708577
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-03 23:59:21 +0000

    short

--- backend/Controllers/authControllers.js
+++ backend/Controllers/authControllers.js
@@ -78,7 +78,29 @@ export const login =async(req, res) =>{
         const patient = await User.findOne({email})
         const doctor = await Doctor.findOne({email})
 
-        
+        if(patient){
+            user = patient
+        }
+        if(doctor){
+            user = doctor
+        }
+
+        // check if user exist or not 
+        if(!user){
+            return res.status(404).json({
+                message: "User not found"
+            })
+        }
+
+        // compare password
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                status: false,
+                message: "User not found"
+            })
+        }
 
     } catch (err) {
         
