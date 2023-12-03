7b8514be87570c504c713c1f51dd95a3df6c4b91
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-03 23:36:44 +0000

    short

--- backend/Controllers/authControllers.js
+++ backend/Controllers/authControllers.js
@@ -51,8 +51,19 @@ export const register =async(req, res) =>{
             })
         }
 
+        await user.save()
+
+        res.status(200).json({
+            success: true,
+            message: 'User successfully created'
+        })
+
+
     } catch (err) {
-        
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error, Try again'
+        })
     }
 }
 
