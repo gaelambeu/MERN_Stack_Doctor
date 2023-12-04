de5c181e0a4e598b623b9bb9578f37857dfd5b39
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-04 14:22:29 +0000

    short

--- backend/Controllers/doctorControllers.js
+++ backend/Controllers/doctorControllers.js
@@ -75,8 +75,23 @@ export const getAllDoctor = async(req, res) => {
     const id = req.params.id
 
     try {
+
+        const {query} = req.query
+        let doctors;
+
+        if(query){
+            doctors =  await Doctor.find({
+                isApproved: "approved", 
+                $or:[
+                    { name:{$regex: query, $options: "i"} }, 
+                    { specialization:{$regex: query, $options: "i"} }
+                ],
+            }).select("-password")
+        } else {
+
+            doctors = await Doctor.find({}).select("-password")
+        }
         
-        const doctors = await Doctor.find({}).select("-password")
 
         res.status(200).json({
             success: true, 
