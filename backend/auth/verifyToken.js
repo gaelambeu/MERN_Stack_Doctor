3dc51977cf45c488f269baae30a5e1eb9638e119
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-06 23:47:13 +0000

    short

--- backend/auth/verifyToken.js
+++ backend/auth/verifyToken.js
@@ -13,9 +13,29 @@ export const authenticate = async (req, res, next) => {
     }
 
     try {
-        console.log(authToken);
-        next();
+        const token = authToken.split("")[1];
+
+        // verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+
+        req.userId = decoded.id
+        req.role = decoded.role
+
+        next();  // must be call the next function
     } catch (err) {
-        
+        if(err.name === 'TokenExpiredError'){
+            return res.status(401).json({ message:'Token is expired' })
+        }
+
+        return res.status(401).json({success:false, message:'Invalid token'})
     }
+}
+
+
+export const restrict = async(req, res, next) => {
+    const userId = req.userId
+
+    let user;
+
+    
 }
