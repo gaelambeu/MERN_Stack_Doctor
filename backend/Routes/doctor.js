358ff6f5a53c14322764f774b022aa067824f1fa
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-07 13:54:45 +0000

    short

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -7,8 +7,12 @@ import {
 } from "../Controllers/doctorControllers.js"
 
 import { authenticate, restrict } from '../auth/verifyToken.js';
+import reviewRouter from './review.js'
 
 const router = express.Router()
+
+// nested route
+router.use('/:doctorId/reviews', reviewRouter)
 
 router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctor)
--- backend/Routes/review.js
+++ backend/Routes/review.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { getAllReviews, createReview } from '../Controllers/reviewControllers.js'
 import { authenticate, restrict } from '../auth/verifyToken.js';
 
-const router = express.Router()
+const router = express.Router({mergeParams: true})
 
 // doctor/doctorId
 
