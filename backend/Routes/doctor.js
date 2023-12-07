1ab5948f1015c623d45c0744bd2b25c08c63016e
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-07 00:34:11 +0000

    short

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -12,7 +12,7 @@ const router = express.Router()
 
 router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctor)
-router.put('/:id', updateDoctor)
-router.delete('/:id', deleteDoctor)
+router.put('/:id', authenticate, restrict(["doctor"]), updateDoctor)
+router.delete('/:id', authenticate, restrict(["doctor"]), deleteDoctor)
 
 export default router;
