a993ffe67f31bebb7d2840a2efeb158e72878de1
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-10 15:10:37 +0000

    short

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,13 @@ import "react-toastify/dist/ReactToastify.css"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ToastContainer 
+      theme="dark" 
+      position="top-right" 
+      autoClose={3000} 
+      closeOnClick 
+      pauseOnHover={false} 
+    />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
