0b08a92c6c409a43a80ab15faf669281ae33a47a
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-10 19:42:47 +0000

    short

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -80,18 +80,34 @@ const Header = () => {
 
           {/* =================== nav right ==================== */}
           <div className="flex items-center gap-4">
-            <div className="hidden">
-              <Link to='/'>
-                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
-                  <img src={userImg} className="w-full rounded-full" alt="" />
-                </figure>
+            {token && user ? (
+              <div>
+                <Link 
+                  to={`${
+                    role==="doctor" 
+                    ? "/doctors/profile/me" 
+                    : "/users/profile/me"
+                  }`}>
+                  <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                    <img 
+                      src={user?.photo} 
+                      className="w-full rounded-full" 
+                      alt="" 
+                    />
+                  </figure>
+                </Link>
+              </div> 
+            ) :  (
+              <Link to='/login'>
+                <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center 
+                justify-center rounded-[50px]">Login</button>
               </Link>
-            </div>
-            <h1>{user?.name}</h1>
-            <Link to='/login'>
-              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center 
-              justify-center rounded-[50px]">Login</button>
-            </Link>
+            )}
+
+
+            
+            
+            
 
             <span className="md:hidden" onClick={toggleMenu}>
               <BiMenu className='w-6 h-6 cursor-pointer' />
