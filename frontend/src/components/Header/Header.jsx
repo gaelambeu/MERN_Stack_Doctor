370fcf8738b4b89b977f0ac50c52fe8f44d47c27
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-11 23:45:31 +0000

    short

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useRef, useContext} from 'react'
 import logo from "../../assets/images/logo.png"
-import userImg from "../../assets/images/avatar-icon.png"
+
 import { NavLink, Link } from 'react-router-dom'
 import { BiMenu } from "react-icons/bi"
 import { authContext } from "../../context/AuthContext.jsx"
@@ -95,7 +95,7 @@ const Header = () => {
                       alt="" 
                     />
                   </figure>  
-                  <h2>{user?.name}</h2>
+                  
                 </Link>
               </div> 
             ) :  (
