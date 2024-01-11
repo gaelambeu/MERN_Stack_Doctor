1d273f9caa9b7a8552185ef7d8aa7e3a5e99906b
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-11 22:20:37 +0000

    short

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -177,14 +177,14 @@ const Signup = () => {
                     onChange={handleFileInputChange}
                     accept=".jpg,.png" 
                     className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
-                />
+                  />
 
-                <label htmlFor="customFile" 
-                className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] 
-                text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg
-                truncate cursor-pointer">
-                    Upload Photo
-                </label>
+                  <label htmlFor="customFile" 
+                  className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] 
+                  text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg
+                  truncate cursor-pointer">
+                      Upload Photo
+                  </label>
                 </div>
               </div>
 
