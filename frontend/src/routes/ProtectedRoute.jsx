8aa6fe526ad57799b3192255fafd90ed2b540b15
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-12 01:37:57 +0000

    short

---
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { authContext } from '../context/AuthContext'
+
+const ProtectedRoute = () => {
+  return (
+    <div>ProtectedRoute</div>
+  )
+}
+
+export default ProtectedRoute
