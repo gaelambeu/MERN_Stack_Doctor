1f3d4e1fd6711ded3f8f770d95faf1ef01d125fa
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-12 00:57:18 +0000

    short

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -3,7 +3,8 @@ import {
     updateDoctor, 
     deleteDoctor, 
     getAllDoctor, 
-    getSingleDoctor
+    getSingleDoctor,
+    getDoctorProfile
 } from "../Controllers/doctorControllers.js"
 
 import { authenticate, restrict } from '../auth/verifyToken.js';
@@ -18,5 +19,7 @@ router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctor)
 router.put('/:id', authenticate, restrict(["doctor"]), updateDoctor)
 router.delete('/:id', authenticate, restrict(["doctor"]), deleteDoctor)
+
+router.get('/profile/me', authenticate, restrict(['doctor']), getDoctorProfile)
 
 export default router;
