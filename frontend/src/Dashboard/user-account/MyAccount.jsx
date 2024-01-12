30cbe35d1cb079748128070df7276775208e8b78
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-12 02:10:23 +0000

    short

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const MyAccount = () => {
   return (
-    <div>MyAccount</div>
+    <div className='max-w-[1170px] px-5 mx-auto'>
+      <div className="grid md:grid-cols-3 gap-10">
+
+        <div className="pb-[50px] px-[30px] rounded-md">
+          <div className="flex items-center justify-center">
+            
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
