09cb6ca0154af45281c86a6593134e0435950dce
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-12 01:06:21 +0000

    short

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -6,7 +6,7 @@ import Services from '../pages/Services'
 import Signup from '../pages/Signup'
 import Doctors from '../pages/Doctors/Doctors'
 import DoctorsDetails from '../pages/Doctors/DoctorsDetails'
-
+import MyAccount from '../Dashboard/user-account/MyAccount'
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -20,6 +20,7 @@ const Routers = () => {
     <Route path="/register" element={<Signup/>} />
     <Route path="/contact" element={<Contact/>} />
     <Route path="/services" element={<Services/>} />
+    <Route path="/users/profile/me" element={<MyAccount/>} />
   </Routes>
 }
 
