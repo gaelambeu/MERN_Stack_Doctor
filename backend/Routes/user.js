68f91e4cc9be3cf5a1a8a3387cdb1a7505f2f0bd
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-12 00:45:13 +0000

    short

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -3,7 +3,9 @@ import {
     updateUser, 
     deleteUser, 
     getAllUser, 
-    getSingleUser
+    getSingleUser,
+    getUserProfile,
+    getMyAppointments
 } from "../Controllers/userControllers.js";
 
 import { authenticate, restrict } from '../auth/verifyToken.js';
@@ -14,6 +16,13 @@ const router = express.Router()
 router.get('/:id', authenticate, restrict(["patient"]), getSingleUser)
 router.get('/', authenticate, restrict(["admin"]), getAllUser)
 router.put('/:id', authenticate, restrict(["patient"]), updateUser)
-router.delete('/:id', authenticate, restrict(["patient"]), deleteUser)
+router.get('/profile/me', authenticate, restrict(["patient"]), getUserProfile)
+router.get(
+    'appointments/my-appointments', 
+    authenticate, 
+    restrict(["patient"]), 
+    getMyAppointments
+);
+
 
 export default router;
