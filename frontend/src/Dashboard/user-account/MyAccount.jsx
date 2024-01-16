9fda905d31dfd08ad009adc1316c3d12f4eb030a
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-16 15:02:22 +0000

    short

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -30,7 +30,12 @@ const MyAccount = () => {
   return (
     <section>
       <div className='max-w-[1170px] px-5 mx-auto'>
-      <div className="grid md:grid-cols-3 gap-10">
+        {loading && <Loading/>}
+
+
+        {
+          !loading && !error && (
+            <div className="grid md:grid-cols-3 gap-10">
 
         <div className="pb-[50px] px-[30px] rounded-md">
           <div className="flex items-center justify-center">
@@ -90,7 +95,9 @@ const MyAccount = () => {
 
 
       </div>
-    </div>
+          )
+        }
+      </div>
     </section>
   )
 }
